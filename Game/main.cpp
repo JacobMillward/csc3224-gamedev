5ea10fbb6fc9d2629af70468e86bc2b2c2354a06
Jacob Millward <j.millward@ncl.ac.uk>
2017-04-22 18:39:39 +0100

    Add comment explaining framerate limit

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,6 +1,9 @@
 #include "MyGame.h"
 #include <iostream>
 
+/*
+ * Depending on the video drivers, the ouput framerate may be limited by the screen refresh rate. There doesn't seem to be anything that can fix this at the moment within SFML.
+ */
 int main()
 {
 	auto app = new MyGame();
