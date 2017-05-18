6caa6669482e05b6c679b64ecac7894065e84549
Jacob Millward <j.millward@ncl.ac.uk>
2017-05-18 06:10:07 +0100

    Added loading entities from JSON files

--- CSC3224-gamedev/FileLoader.cpp
+++ CSC3224-gamedev/FileLoader.cpp
@@ -2,55 +2,77 @@
 #include <fstream>
 #include <iostream>
 #include "Components/Tag.h"
+#include "Components/PhysicsBody.h"
+#include "Components/SoundEffect.h"
 
 
 FileLoader::~FileLoader()
 {
 }
 
-void FileLoader::LoadEntitiesFromFile(EntityManager& entityManager, std::string filePath)
+void FileLoader::LoadEntitiesFromFile(EntityManager& entityManager, PhysicsSystem& physicsSystem, string filePath)
 {
-	/* INCOMPLETE */
-	std::ifstream ifs(filePath);
-	std::string content((std::istreambuf_iterator<char>(ifs)),
-		(std::istreambuf_iterator<char>()));
+	ifstream ifs(filePath);
+	string content((istreambuf_iterator<char>(ifs)),
+	               (istreambuf_iterator<char>()));
 
 	Json::Value root;
 	Json::Reader reader;
-	
-	if(!reader.parse(content, root))
+
+	if (!reader.parse(content, root))
 	{
-		std::cout << "Failed to load config" << std::endl;
+		cout << "Failed to load config" << endl;
 		return;
 	}
 
 	if (root.size() > 0)
 	{
-
 		//For each entity
-		for (Json::ValueIterator itr = root.begin(); itr != root.end(); ++itr) {
-			auto entity = entityManager.createEntity(itr.key()["Sprite"]["TextureID"].asString(),
-				sf::IntRect(root["Sprite"]["rectTL"].asInt(),
-					itr.key()["Sprite"]["rectTR"].asInt(),
-					itr.key()["Sprite"]["width"].asInt(),
-					itr.key()["Sprite"]["height"].asInt()));
+		for (auto jsonValue : root)
+		{
+			auto entity = entityManager.createEntity(jsonValue["Sprite"]["TextureID"].asString(),
+			                                         sf::IntRect(root["Sprite"]["rectTL"].asInt(),
+			                                                     jsonValue["Sprite"]["rectTR"].asInt(),
+			                                                     jsonValue["Sprite"]["width"].asInt(),
+			                                                     jsonValue["Sprite"]["height"].asInt()));
 			//Loop though components
-			for (auto component : itr.key()) {
-				auto type = component.get("Type", "-1").asInt();
-				if (type < 0) {
-					std::cout << "Invalid component type" << std::endl;
+			for (auto jsonComponent : jsonValue)
+			{
+				auto type = jsonComponent.get("ComType", -1).asInt();
+				if (type < 0)
+				{
+					cout << "Invalid component type" << endl;
 					break;
 				}
 				auto comType = static_cast<ComponentType>(type);
-				switch (comType) {
+
+				Component* component = nullptr;
+				switch (comType)
+				{
+				case ComponentType::TAG:
+					{
+						component = Tag::buildFromJson(jsonComponent);
+						break;
+					}
+				case ComponentType::PHYSICSBODY:
+					{
+						component = PhysicsBody::buildFromJson(jsonComponent);
+						break;
+					}
+				case ComponentType::AUDIO:
+					{
+						component = SoundEffect::buildFromJson(jsonComponent);
+						break;
+					}
+
+				//We must have a sprite, so don't bother creating additional ones (undefined behaviour)
 				case ComponentType::SPRITE: break;
-				case ComponentType::TAG: 
-					entity->addComponent(*Tag::buildFromJson(component));
-					break;
-				case ComponentType::PHYSICSBODY: break;
-				case ComponentType::AUDIO: break;
-				case ComponentType::TYPE_END: break;
+				case ComponentType::TYPE_END:
 				default: ;
+				}
+				if(component)
+				{
+					entity->addComponent(*component);
 				}
 			}
 		}
--- CSC3224-gamedev/FileLoader.h
+++ CSC3224-gamedev/FileLoader.h
@@ -2,11 +2,13 @@
 #include "json/json.h"
 #include "EntityManager.h"
 
+class PhysicsSystem;
+
 class FileLoader
 {
 public:
 	~FileLoader();
 
-	static void LoadEntitiesFromFile(EntityManager& entityManager, std::string filePath);
+	static void LoadEntitiesFromFile(EntityManager& entityManager, PhysicsSystem& physicsSystem, std::string filePath);
 };
 
